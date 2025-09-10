edb690869bc688efcdd8c719cd87c29796471eca
Christian Ramelow <info@codekandis.net>
2025-09-10 23:34:33 +0200

    Initial commit.

---
+++ tests/bootstrap.php
@@ -0,0 +1,23 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\ToolKit\Tests;
+
+use function dirname;
+use function error_reporting;
+use function ini_set;
+use function sprintf;
+use const E_ALL;
+
+/**
+ * Represents the bootstrap script of the tests.
+ * @package codekandis/toolkit
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+error_reporting( E_ALL );
+ini_set( 'display_errors', 'On' );
+ini_set( 'html_errors', 'Off' );
+
+require_once sprintf(
+	'%s/%s',
+	dirname( __DIR__, 1 ),
+	'vendor/autoload.php'
+);
